d707bbd8b9f99a491c5954ea1085a7d134975cf7
Roman <rsheyko1997@gmail.com>
2021-10-10 19:13:47 +0300

    first commit

---
+++ RenderSystem.cpp
@@ -0,0 +1,8 @@
+//
+//  RenderSystem.cpp
+//  FluidSimulation
+//
+//  Created by Roman on 10.10.2021.
+//
+
+#include "RenderSystem.hpp"
---
+++ RenderSystem.hpp
@@ -0,0 +1,13 @@
+//
+//  RenderSystem.hpp
+//  FluidSimulation
+//
+//  Created by Roman on 10.10.2021.
+//
+
+#ifndef RenderSystem_hpp
+#define RenderSystem_hpp
+
+#include <stdio.h>
+
+#endif /* RenderSystem_hpp */
