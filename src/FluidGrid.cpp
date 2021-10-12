e725194df1977a5582f0659c84cc95950265c76d
Roman <rsheyko1997@gmail.com>
2021-10-13 01:01:49 +0300

    Add V and U velocities

--- src/FluidGrid.cpp
+++ src/FluidGrid.cpp
@@ -11,7 +11,8 @@ FluidGrid::FluidGrid(int gridWidth, int gridHeight) {
 
 void FluidGrid::SetCellInfoAt(const glm::vec2 &pos, const FluidGrid::CellInfo &cellInfo) { 
    grid[pos.y][pos.x].pressure = cellInfo.pressure;
-   grid[pos.y][pos.x].velocity = cellInfo.velocity;
+   grid[pos.y][pos.x].velocityU = cellInfo.velocityU;
+   grid[pos.y][pos.x].velocityV = cellInfo.velocityV;
 }
 
 
@@ -20,7 +21,7 @@ void FluidGrid::SetPressureAt(const glm::vec2 &pos, float pressure) {
 }
 
 
-void FluidGrid::SetVelocityAt(const glm::vec2 &pos, const glm::vec2 &velocity) { 
-   grid[pos.y][pos.x].velocity = velocity;
+void FluidGrid::SetVelocityAt(const glm::vec2 &pos, const glm::vec2& velocityU, const glm::vec2& velocityV) {
+   grid[pos.y][pos.x].velocityU = velocityU;
+   grid[pos.y][pos.x].velocityV = velocityV;
 }
-
--- src/FluidGrid.hpp
+++ src/FluidGrid.hpp
@@ -1,5 +1,5 @@
-#ifndef FluidSimulation_hpp
-#define FluidSimulation_hpp
+#ifndef FluidGrid_hpp
+#define FluidGrid_hpp
 #include <vector>
 #include <iostream>
 #define GLM_FORCE_RADIANS
@@ -10,15 +10,16 @@ public:
    FluidGrid(int gridWidth, int gridHeight);
    
    struct CellInfo {
-      glm::vec2 velocity;
+      glm::vec2 velocityU;
+      glm::vec2 velocityV;
       float pressure = 0.f;
    };
    
-   void SetVelocityAt(const glm::vec2& pos, const glm::vec2& velocity);
+   void SetVelocityAt(const glm::vec2& pos, const glm::vec2& velocityU, const glm::vec2& velocityV);
    void SetPressureAt(const glm::vec2& pos, float pressure);
    void SetCellInfoAt(const glm::vec2& pos, const CellInfo& cellInfo);
 private:
    std::vector<std::vector<CellInfo>> grid;
 };
 
-#endif /* FluidSimulation_hpp */
+#endif /* FluidGrid_hpp */
